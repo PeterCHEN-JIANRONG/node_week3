c002cac6162989efa5241187ef576feb50af841f
peter.chen1024 <u0052017@gmail.com>
2022-05-02 13:11:26 +0800

    add cors

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors"); // 跨網域設定 cors
 
+// router
 var indexRouter = require("./routes/index");
 var postsRouter = require("./routes/posts");
 
@@ -11,6 +13,7 @@ var app = express();
 // Database 資料庫連線
 require("./connections");
 
+app.use(cors()); // 啟用 cors 跨網域設定
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
